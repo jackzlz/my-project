6581944e2c525c3a9bcd85c25646b8eec66c85b1
jackzlz <jackzlz@126.com>
2018-04-11 22:46:01 +0800

    my-project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <p>导航：
+       <router-link to="/">首页</router-link>
+       <router-link to="/hi">Hi页面</router-link>
+    </p>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
